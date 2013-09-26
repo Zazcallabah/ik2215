dfcf8aebdb0b867eeb8c62cdd400eb85184db018
Peter Hamberg <zazcallabah@gmail.com>
2013-09-26 12:19:58 +0200

    add comments explaining script

--- dhcp3.sh
+++ dhcp3.sh
@@ -1,8 +1,11 @@
 # https://help.ubuntu.com/community/dhcp3-server
 
-#testing purpose
-
+# Silently install dhcp server
 export DEBIAN_FRONTEND=noninteractive
 apt-get -q -y install isc-dhcp-server
+
+# Copy the configuration files to server
 cp ./conf/dhcp3.conf /etc/dhcp3/dhcpd.conf
+
+# Start the service
 /etc/init.d/isc-dhcp-server start
