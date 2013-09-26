e0d895cdf3da1111c820c6e8b616e5e05c9a54ac
Peter Hamberg <zazcallabah@gmail.com>
2013-09-26 12:49:33 +0200

    add comments to dns script

--- dns-bind.sh
+++ dns-bind.sh
@@ -1,8 +1,11 @@
 # https://help.ubuntu.com/community/BIND9ServerHowto#Primary_Master_Server
 
+#install service
 export DEBIAN_FRONTEND=noninteractive
 apt-get -q -y install bind9
 
+# copy configuration files
 cp ./conf/named.conf* /etc/bind/
 
+# start service
 service bind9 start
