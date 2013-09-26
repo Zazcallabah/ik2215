61060bbde158337c8f4f74def8465057564eaeff
Peter Hamberg <zazcallabah@gmail.com>
2013-09-26 17:08:38 +0200

    add a cleaner conf and install script

--- nginx.sh
+++ nginx.sh
@@ -1,18 +1,12 @@
-#!/bin/bash
+
 # install service
-export DEBIAN_FRONTEND=noninteractive
 apt-get -q -y install nginx
 
-echo "\n\nEnter password for nginx user"
-read pass
-useradd nginxuser
-echo "$pass
-$pass" | sudo passwd nginxuser
+# copy configuration
+cp conf/nginx.conf /etc/nginx/sites-enabled/default
 
-#update this location to be actual nginx config location
-cp conf/upstart-nginx.conf /etc/init/nginx.conf
-cp conf/nginx.conf /etc/nginx/nginx.conf
-mkdir -p /var/www/default
+# create web directory (matches path in conf/nginx.conf)
+mkdir -p /home/www
 
 #start service
 service nginx start
