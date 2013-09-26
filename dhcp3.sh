93ed73166c034683e984ec9665a403069d26f5ad
Peter Hamberg <zazcallabah@gmail.com>
2013-09-26 11:41:45 +0200

    correct way to start dhcp service

--- dhcp3.sh
+++ dhcp3.sh
@@ -3,4 +3,4 @@
 export DEBIAN_FRONTEND=noninteractive
 apt-get -q -y install isc-dhcp-server
 cp ./conf/dhcp3.conf /etc/dhcp3/dhcpd.conf
-service dhcp3-server start
+/etc/init.d/isc-dhcp-server start
