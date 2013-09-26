3bb9f538feb714761324f6cc0b87380ef2978f7d
SoumyaParida <soumya.parida3@gmail.com>
2013-09-26 12:11:41 +0200

    test edit dhcp file

--- dhcp3.sh
+++ dhcp3.sh
@@ -1,5 +1,7 @@
 # https://help.ubuntu.com/community/dhcp3-server
 
+#testing purpose
+
 export DEBIAN_FRONTEND=noninteractive
 apt-get -q -y install isc-dhcp-server
 cp ./conf/dhcp3.conf /etc/dhcp3/dhcpd.conf
