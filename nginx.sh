e7a776a723df85cd65d8ab15c5270b366506938d
Peter Hamberg <zazcallabah@gmail.com>
2013-09-26 13:14:56 +0200

    add startup script for nginx

--- nginx.sh
+++ nginx.sh
@@ -3,6 +3,7 @@ export DEBIAN_FRONTEND=noninteractive
 apt-get -q -y install nginx
 
 #update this location to be actual nginx config location
+cp conf/upstart-nginx.conf /etc/init/nginx.conf
 cp conf/nginx.conf /etc/nginx/nginx.conf
 mkdir -p /var/www/default
 
