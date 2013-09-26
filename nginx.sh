f807130ed3d8ca89c92f46c4b8ec8b843a36cdee
Peter Hamberg <zazcallabah@gmail.com>
2013-09-26 13:37:57 +0200

    handle nginx user administration

--- nginx.sh
+++ nginx.sh
@@ -1,6 +1,13 @@
+#!/bin/bash
 # install service
 export DEBIAN_FRONTEND=noninteractive
 apt-get -q -y install nginx
+
+echo "\n\nEnter password for nginx user"
+read pass
+useradd nginxuser
+echo "$pass
+$pass" | sudo passwd nginxuser
 
 #update this location to be actual nginx config location
 cp conf/upstart-nginx.conf /etc/init/nginx.conf
