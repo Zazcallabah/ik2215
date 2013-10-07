7f657128278c2d2a89b35adbdee6ffce691cd622
Peter Hamberg <zazcallabah@gmail.com>
2013-10-07 17:00:19 +0200

    remove copy-paste cruft from script

--- mail-sasl.sh
+++ mail-sasl.sh
@@ -1,18 +1,7 @@
-# http://wiki2.dovecot.org/FrontPage
-# https://help.ubuntu.com/community/Dovecot
-
 #install service
 export DEBIAN_FRONTEND=noninteractive
-apt-get -q -y install dovecot-imapd dovecot-pop3d
+apt-get -q -y install sasl2-bin
 
 # copy configuration files
-cp ./conf/dovecot.conf /etc/dovecot/dovecot.conf
-
-# setup user accounts
-# http://wiki2.dovecot.org/UserIds#mailusers
-
-
-
 
 # start service
-/etc/init.d/dovecot start
