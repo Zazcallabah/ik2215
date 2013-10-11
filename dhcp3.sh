92dce287011c08a3a0e67e63ee54b3550ccc7d01
Peter Hamberg <zazcallabah@gmail.com>
2013-10-11 10:32:57 +0200

    update dhcp install script

--- dhcp3.sh
+++ dhcp3.sh
@@ -1,11 +1,19 @@
+#!/bin/bash
 # https://help.ubuntu.com/community/dhcp3-server
 
-# Silently install dhcp server
-export DEBIAN_FRONTEND=noninteractive
-apt-get -q -y install isc-dhcp-server
+if [ "$1" == "install" ]
+then
+	#install service
+	export DEBIAN_FRONTEND=noninteractive
+	apt-get -q -y install isc-dhcp-server
+fi
 
 # Copy the configuration files to server
-cp ./conf/dhcp3.conf /etc/dhcp3/dhcpd.conf
+cp ./conf/dhcpd.conf /etc/dhcp3/dhcpd.conf
+
+# configure static ip for dhcp server
+ifconfig eth0 10.2.8.22 netmask 255.255.255.240
+route add default gw 10.2.8.19
 
 # Start the service
 /etc/init.d/isc-dhcp-server start
