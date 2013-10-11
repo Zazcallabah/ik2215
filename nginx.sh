03be906f5a9439871a3901c4c274a21988d4bca3
Peter Hamberg <zazcallabah@gmail.com>
2013-10-11 11:50:07 +0200

    add content for the web server

--- nginx.sh
+++ nginx.sh
@@ -7,6 +7,8 @@ cp conf/nginx.conf /etc/nginx/sites-enabled/default
 
 # create web directory (matches path in conf/nginx.conf)
 mkdir -p /home/www
+tar -xvf conf/particle.tar.gz
+cp particle/* /home/www
 
 #start service
 service nginx start
