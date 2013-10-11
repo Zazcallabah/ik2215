7e2021f51b0d38895c05a8f4a7801922dc8946a3
Peter Hamberg <zazcallabah@gmail.com>
2013-10-11 10:23:14 +0200

    update install script to copy correct files

--- dns-bind.sh
+++ dns-bind.sh
@@ -1,11 +1,21 @@
+#!/bin/bash
 # https://help.ubuntu.com/community/BIND9ServerHowto#Primary_Master_Server
 
-#install service
-export DEBIAN_FRONTEND=noninteractive
-apt-get -q -y install bind9
+if [ "$1" == "install" ]
+then
+	#install service
+	export DEBIAN_FRONTEND=noninteractive
+	apt-get -q -y install bind9
+fi
 
 # copy configuration files
-cp ./conf/named.conf* /etc/bind/
+cp ./conf/bind/db.isp2.lab /etc/bind/
+cp ./conf/bind/named.conf.options /etc/bind/
+cp ./conf/bind/named.conf.local /etc/bind/
+
+chmod a+r /etc/bind/db.isp2.lab
+chmod a+r /etc/bind/named.conf.options 
+chmod a+r /etc/bind/named.conf.local
 
 # start service
 service bind9 start
