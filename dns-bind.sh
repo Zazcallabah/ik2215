541c43fca01f0e4723f0e24f9de3ec7f84a74b1e
Peter Hamberg <zazcallabah@gmail.com>
2013-10-14 13:44:29 +0200

    update dns setup script to install missing file

--- dns-bind.sh
+++ dns-bind.sh
@@ -12,10 +12,12 @@ fi
 cp ./conf/bind/db.isp2.lab /etc/bind/
 cp ./conf/bind/named.conf.options /etc/bind/
 cp ./conf/bind/named.conf.local /etc/bind/
+cp ./conf/bind/db.10.2.0 /etc/bind/
 
 chmod a+r /etc/bind/db.isp2.lab
 chmod a+r /etc/bind/named.conf.options 
 chmod a+r /etc/bind/named.conf.local
+chmod a+r /etc/bind/db.10.2.0
 
 # start service
 service bind9 start
