223125f34d2940fa81c28bd33162d405c1aad813
Peter Hamberg <zazcallabah@gmail.com>
2013-10-17 16:07:20 +0200

    update web server script to use new content

--- nginx.sh
+++ nginx.sh
@@ -6,9 +6,14 @@ apt-get -q -y install nginx
 cp conf/nginx.conf /etc/nginx/sites-enabled/default
 
 # create web directory (matches path in conf/nginx.conf)
-mkdir -p /home/www
+mkdir -p /home/www/particle
 tar -xvf conf/particle.tar.gz
-cp particle/* /home/www
+cp particle/* /home/www/particle
+cp conf/final2.pdf /home/www
+cp conf/final2.html /home/www
+cp conf/topology.png /home/www
+
+
 
 #start service
 service nginx start
